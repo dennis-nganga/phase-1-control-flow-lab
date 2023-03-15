8feb6d27827a673b2275dc05ce7b75317429e8b0
dennis kimani <denniskimani416@gmail.com>
2023-03-15 08:29:32 +0300

    lab on control flow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(_distanceInFeet){
+  if (_distanceInFeet<=400){
+    return `This one is on me!`
+  } else if (_distanceInFeet > 2000 && _distanceInFeet<= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  } 
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const response = city === "NYC"?"Ok, sounds good." : "No go.";
+return response;  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
